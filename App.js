c01d1daf9c91b7aa49b8d1cf0307d0ef5afdd779
Rohanrana2003 <rohanrana33042@gmail.com>
2024-09-12 15:19:09 +0530

    Adding Restaurant Cards

--- App.js
+++ App.js
@@ -1,19 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//Element
-const reactElement = <h1>This is an React Element</h1>
 
-//Component
-const ReactComponent = () => {
-   return <h1>This is an React Component</h1>
+
+const Header = () =>{
+    return(
+        <div className='header'>
+            <div className='logo-container'>
+                <img 
+                 className='logo'
+                 src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All" alt="" />
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+
+                </ul>
+            </div>
+        </div>
+    )
 }
 
-//For Rendering Element
+const RestaurantCard = () =>{
+    return(
+        <div className="res-card">
+            <img
+             className='res-logo'
+             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rvxp5xbniat84r6efku2" alt="card-img" />
+            <h3>Meghna Foods</h3>
+            <h4>North Indian, Biryani, Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+
+        </div>
+    )
+}
+
+const Body = () =>{
+    return (
+        <div className="body">
+
+            <div className="search">
+                Search
+            </div>
+
+            <div className="rest-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+
+            </div>
+
+        </div>
+    )
+}
+
+
+
+ const AppLayout = () =>{
+    return(
+       
+        <div className="app">
+
+            <Header/>
+            <Body/>
+
+        </div>
+
+    )
+ }
+
+
+
 ReactDOM.createRoot(document.getElementById('root'))
-.render(reactElement);
-
-
-//For Rendering Component
-// ReactDOM.createRoot(document.getElementById('root'))
-// .render(<ReactComponent/>);
+.render(<AppLayout/>)
